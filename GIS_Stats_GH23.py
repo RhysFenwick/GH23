705c586634d3219bcf911968f7c73a2622f9ce46
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-08-19 13:26:22 +1000

    fixed the map

--- GIS_Stats_GH23.py
+++ GIS_Stats_GH23.py
@@ -3,11 +3,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Read the shapefile
-gdf = gpd.read_file(r"C:\Users\Rhys\Downloads\SA2_2021_AUST_SHP_GDA2020\SA2_2021_AUST_GDA2020.shp")
-SA_data = pd.read_csv(r"C:\Users\Rhys\Downloads\BirthsLevel2Cleaned.csv")
-merged = gdf.set_index('SA2_CODE21').join(SA_data.set_index('SA2'))
-# print(merged)
+gdf = gpd.read_file(r"C:\Users\61411\OneDrive\Desktop\Govhack\SA2_2021_AUST_GDA2020.shp")
+gdf = gdf.set_index('SA2_CODE21')
 
+SA_data = pd.read_csv("BirthsLevel2Cleaned.csv")
+
+indexes = (SA_data["SA2_CODE21"])
+new_indexes = []
+for x in indexes:
+    new_indexes.append(str(x))
+
+SA_data["SA2_CODE21"] = new_indexes
+SA_data = SA_data.set_index("SA2_CODE21")
+
+merged = (gdf.join(SA_data))
 
 merged.plot(figsize=(5,5), column="Births - 2019")
 plt.show()
