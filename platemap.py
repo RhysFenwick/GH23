d9449d540d52179b0dc83f90b0a01f0620d024d9
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-08-19 17:34:42 +1000

    Plots based on location

--- platemap.py
+++ platemap.py
@@ -6,17 +6,17 @@ import pandas as pd
 gdf = gpd.read_file(r"C:\Users\61411\OneDrive\Desktop\Govhack\SA2_2021_AUST_GDA2020.shp")
 gdf = gdf.set_index('SA2_CODE21')
 
-SA_data = pd.read_csv("BirthsLevel2Cleaned.csv")
+SA_data = pd.read_csv("scores.csv")
 
-indexes = (SA_data["SA2_CODE21"])
+indexes = (SA_data["Location"])
 new_indexes = []
 for x in indexes:
     new_indexes.append(str(x))
 
-SA_data["SA2_CODE21"] = new_indexes
-SA_data = SA_data.set_index("SA2_CODE21")
+SA_data["Location"] = new_indexes
+SA_data = SA_data.set_index("Location")
 
 merged = gdf.join(SA_data)
 
-merged.plot(figsize=(5,5), column="Births - 2019")
+merged.plot(figsize=(5,5), column="Score")
 plt.show()
