336c4df03745af842d02f482cdc516e4c8433a4f
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-08-19 18:37:20 +1000

    The function now prints out the name of the best 5 towns

--- scoring.py
+++ scoring.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
 
+names = pd.read_csv("SA2_decoder.csv")
+dictionary = names.set_index('SA2').to_dict()['Name']
+
 birth = pd.read_csv("normalised_birth_data.csv")
 population = pd.read_csv("normalised_population_data.csv")
 SES = pd.read_csv("normalised_SES_data.csv")
+# user_weights = [0.5,1.5,1]
 
 new_birth = []
 new_population = []
@@ -19,6 +23,7 @@ for location in SES["SA2"]:
 
 new = pd.DataFrame({"Location": new_locations,"Births":new_birth,"Population":new_population,"SES":new_SES})
 overall_score = new["Births"] * new["Population"] * new["SES"]
+# overall_score = new["Births"]**user_weights[0] * new["Population"]**user_weights[1] * new["SES"]**user_weights[2]
 score_dataframe = pd.DataFrame({"Location": new_locations, "Score": overall_score})
 score_dataframe.to_csv("scores.csv",index=False)
 
@@ -26,4 +31,4 @@ max_scores = np.argsort(overall_score).to_list()[-5:]
 best_values = (score_dataframe.loc[max_scores]["Location"].to_list())
 print("The best places to set up are:")
 for x in best_values:
-    print(x)
+    print(dictionary[x])
